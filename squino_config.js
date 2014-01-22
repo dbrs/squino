1c00a892c737a7923aa326121a211d44480662b1
Sean Lensborn <sean.lensborn@gmail.com>
2014-01-22 12:59:43 -0500

    config changes

---
+++ squino_config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.prodpad_key = 'insert key';
+config.helpscout_key = 'insert key';
+config.inbound_key = 'insert key';
+config.port = process.env.WEB_PORT || 8000;
+
+module.exports = config;
