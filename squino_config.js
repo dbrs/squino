e1266064d40d0408107cfd9cd2615a798072056a
Sean Lensborn <sean.lensborn@gmail.com>
2014-01-22 22:24:32 -0500

    addition of urls/keys to support libs

--- squino_config.js
+++ squino_config.js
@@ -1,8 +1,14 @@
 var config = {}
 
+config.prodpad_url = 'https://app.prodpad.com';
 config.prodpad_key = 'insert key';
 config.helpscout_key = 'insert key';
+config.helpscout_url = 'https://api.helpscout.net';
 config.inbound_key = 'insert key';
+config.youtrack_url = 'http://help.dbrs.local';
+config.youtrack_user = 'DBRS2\\youtrack';
+config.youtrack_password = 'insert pass';
+
 config.port = process.env.WEB_PORT || 8000;
 
 module.exports = config;
