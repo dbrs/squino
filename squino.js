9ce4992f72b1c275b6cf6acdaf85a37e62a94947
Tech SMK <techsmk@dbrs.com>
2018-09-22 11:38:16 -0400

    creates youtrack ticket with complete helpscout body in markdown

--- squino.js
+++ squino.js
@@ -12,7 +12,7 @@ var config	    	= require('./squino_config');
 var YouTrackAPI 	= require('./lib/youtrackapi');
 var HelpScoutAPI 	= require('./lib/helpscoutapi');
 var ProdPadAPI   	= require('./lib/prodpadapi');
-
+var TurndownService = require('turndown');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.get( '/prodpad/feedback', function( req, res ) {
 app.get( '/youtrack/create/ist', function( req, res ) {
 	runYouTrack( req, res, config.youtrack_project_1, config.youtrack_project_1_workflow);
 });
-app.get( '/youtrack/create/inf', function( req, res ) {
+app.get( '/youtrack/create/ICE', function( req, res ) {
 	runYouTrack( req, res, config.youtrack_project_2, config.youtrack_project_2_workflow);
 });
 
@@ -36,20 +36,41 @@ function runYouTrack ( servRequest, servResponse, project, workflowId )
 {
 
 	var youtrack = new YouTrackAPI( config.youtrack_url );
+
 	var helpscout = new HelpScoutAPI( config.helpscout_url, config.helpscout_key );
 
 	var conversationId = servRequest.query.id;
 	
-	helpscout.getConversation( conversationId, function( err, data ) 
-	{	
-		if (err)	
-        	{
-        		servResponse.send( 
-				"An error occurred with the HelpScout API, try again later: "+err
-  			);
-  			return;
-        	}
+	helpscout.getConversation( conversationId, function( err, data ) {	
+		if (err) {
+			servResponse.send("An error occurred with the HelpScout API, try again later: " + err);
+			return;
+		}
 
+		var minId = 0;
+		var threadIndex = 0;
+		var minThreadIndex = 0;
+
+		data.item.threads.forEach(function(threadElem){
+			if ( minId == 0 ){
+				minId = threadElem.id;
+				minThreadIndex = threadIndex;
+			} else if ( threadElem.id < minId ) {
+				minId = threadElem.id;
+				minThreadIndex = threadIndex;
+			}
+			console.log(threadElem.id);
+			threadIndex++;
+		})
+		console.log( 'Found minId:' + minId );
+		console.log('Min Thread Index:' + minThreadIndex );
+		console.log(data.item.threads[minThreadIndex])
+
+		// servResponse.send(
+		// 	data.item.threads[minThreadIndex].body
+		// );
+
+		
 		youtrack.login( config.youtrack_user, config.youtrack_password, function(err, cookie)
 			{
 
@@ -61,24 +82,44 @@ function runYouTrack ( servRequest, servResponse, project, workflowId )
 
 				youtrack.cookie = cookie;	
 
+				//console.log(data.item.threads.body)
+
+				
+
+				var turndownService = new TurndownService()
+				var bodyMarkdown = turndownService.turndown(data.item.threads[minThreadIndex].body)
+
+
+
 				youtrack.createIssue( 
 					project, 
 					data.item.subject, 
-					data.item.preview + '\r\nImported From: https://secure.helpscout.net/conversation/'+conversationId, 
-					function( err, body)
+					bodyMarkdown + '\r\nImported From: https://secure.helpscout.net/conversation/'+conversationId, 
+					function( err, body, ticketUrl)
 					{			
 						console.log( 'Posted Issue to YouTrack:'+data.item.subject);
-						helpscout.executeWorkflow( workflowId, conversationId, function( err, dataWorkflow )
-						{
-							console.log( 'executed workflow error code(' + err +')');
-							servResponse.send(  
-								"<head><script type='text/javascript'> window.location='https://secure.helpscout.net/conversation/"+conversationId+"';</script></head>"
-  					 		);
-						});						
+
+						var ticketNonRestUrl = ticketUrl.replace("/rest", "");
+
+						servResponse.send(  
+								"<head><script type='text/javascript'> window.location='" + ticketNonRestUrl + "';</script></head>"
+						);
+
+						return;
+						
+						// Commenting out HelpScout workflor for now. Let's first get YouTrack ticket creation working.
+
+						// helpscout.executeWorkflow( workflowId, conversationId, function( err, dataWorkflow )
+						// {
+						// 	console.log( 'executed workflow error code(' + err +')');
+						// 	servResponse.send(  
+						// 		"<head><script type='text/javascript'> window.location='https://secure.helpscout.net/conversation/"+conversationId+"';</script></head>"
+  					 	// 	);
+						// });						
 					} 
 				);	
 
-			});			
+			});		
 	} )
 }
 
--- squino_config.js
+++ squino_config.js
@@ -2,15 +2,15 @@ var config = {}
 
 config.prodpad_url 				= 'https://app.prodpad.com';
 config.prodpad_key 				= 'INSERT PRODPAD KEY';
-config.helpscout_key				= 'INSERT HELPSCOUT KEY';
+config.helpscout_key				= '2c16476d1c00e8a3fee8d1f7fab3aeb5a8c47978';
 config.helpscout_url 				= 'https://api.helpscout.net';
 config.inbound_key 				= 'insert key';
-config.youtrack_url 				= 'http://youtrack.com/url/';
-config.youtrack_user 				= 'YOUTRACK_USER';
-config.youtrack_password 			= 'YOUTRACK_PASS';
+config.youtrack_url 				= 'http://help.dbrs.local';
+config.youtrack_user 				= 'smankoo@dbrs.com';
+config.youtrack_password 			= 'Sm123456';
 config.youtrack_project_1			= '3 Letter Name of Project 1';
 config.youtrack_project_1_workflow 		= 1; // workflow id to execute for project 1
-config.youtrack_project_2			= '3 Letter Name of Project 2';
+config.youtrack_project_2			= 'ICE';
 config.youtrack_project_2_workflow		= 2; // Workflow id to execute for project 2
 
 config.port = process.env.WEB_PORT || 8000;
