0b3a59628f74c7a66722a105d64b7fd267b076e4
Tech SMK <techsmk@dbrs.com>
2018-09-23 10:13:51 -0400

    removed credentials

--- squino_config.js
+++ squino_config.js
@@ -2,16 +2,15 @@ var config = {}
 
 config.prodpad_url 				= 'https://app.prodpad.com';
 config.prodpad_key 				= 'INSERT PRODPAD KEY';
-config.helpscout_key				= '2c16476d1c00e8a3fee8d1f7fab3aeb5a8c47978';
+config.helpscout_key				= 'INSERT HELPSCOUT KEY';
 config.helpscout_url 				= 'https://api.helpscout.net';
-config.inbound_key 				= 'insert key';
+config.inbound_key 				= '----';
 config.youtrack_url 				= 'http://help.dbrs.local';
-config.youtrack_user 				= 'smankoo@dbrs.com';
-config.youtrack_password 			= 'Sm123456';
-config.youtrack_project_2			= 'ICE';
-config.youtrack_project_2_workflow		= 407326; // Workflow id to execute for project 2
+config.youtrack_user 				= 'Youtrack User';
+config.youtrack_password 			= 'Youtrack Password';
+config.youtrack_project_2			= 'ICE'; //Don't change
+config.youtrack_project_2_workflow		= 407326; // Workflow id to execute
 config.youtrack_default_timezone             = 'America/Toronto' //Timezone to use for printing date in Youtrack Ticket
-
 
 config.port = process.env.WEB_PORT || 8000;
 
